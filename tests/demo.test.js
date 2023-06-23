098ba9c667f935e48b662a9cdac575b45b94f9e6
Lucile16 <lulubeucher@gmail.com>
2023-06-23 15:53:20 +0200

    test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -6,6 +6,6 @@ describe("Checking if the user is created correctly", function () {
     it("should create the user with the correct name", function () {
         debugger
         var tom = new User("tom");
-        should(tom.name).be.equal('tom');
+        should(tom.name).be.equal('tom'); //should used
     });
 });
